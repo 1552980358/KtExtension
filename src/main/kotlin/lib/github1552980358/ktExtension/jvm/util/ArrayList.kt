fa02e2262cca2799843d962dbed3e979325b3162
1552980358 <1552980358@qq.com>
2020-08-01 12:07:24 +0800

    ArrayList.kt: Add `binSearch`
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- src/main/kotlin/lib/github1552980358/ktExtension/jvm/util/ArrayList.kt
+++ src/main/kotlin/lib/github1552980358/ktExtension/jvm/util/ArrayList.kt
@@ -27,3 +27,135 @@ fun <T> ArrayList<T>.copyAndShuffle() = copy().apply { shuffle() }
  * Move element of index [i] into index [j]
  **/
 fun <T> ArrayList<T>.moveAndShift(i: Int, j: Int) = add(j, removeAt(i))
+
+/**
+ * Advanced searching method for [String]
+ **/
+fun ArrayList<String>.binSearch(value: String, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}
+
+/**
+ * Advanced searching method for [Char]
+ **/
+fun ArrayList<Char>.binSearch(value: Char, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}
+
+/**
+ * Advanced searching method for [Int]
+ **/
+fun ArrayList<Int>.binSearch(value: Int, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}
+
+/**
+ * Advanced searching method for [Long]
+ **/
+fun ArrayList<Long>.binSearch(value: Long, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}
+
+/**
+ * Advanced searching method for [Float]
+ **/
+fun ArrayList<Float>.binSearch(value: Float, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}
+
+/**
+ * Advanced searching method for [Double]
+ **/
+fun ArrayList<Double>.binSearch(value: Double, sort: Boolean = false): Int {
+    if (sort) sort()
+    
+    var first = 0
+    var last = lastIndex
+    var mid = lastIndex / 2
+    
+    while ((this[mid] > value || this[mid] < value) || first > last) {
+        if (this[mid] > value) {
+            last = mid
+        } else if (this[mid] < value) {
+            first = mid
+        }
+        mid = (first + last) / 2
+    }
+    
+    return if (this[mid] == value) mid else -1
+}

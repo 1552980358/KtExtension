1bb7868d8c4cbfd88fdc2bb86af8a7df830635dc
1552980358 <1552980358@qq.com>
2020-08-25 18:18:04 +0800

    Add Process.kt
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ src/main/kotlin/lib/github1552980358/ktExtension/jvm/lang/Process.kt
@@ -0,0 +1,42 @@
+@file:Suppress("unused")
+
+package lib.github1552980358.ktExtension.jvm.lang
+
+import lib.github1552980358.ktExtension.jvm.keyword.tryCatch
+
+/**
+ * Write [command] and press enter
+ **/
+fun Process.writeCommand(command: String) = apply {
+    outputStream.bufferedWriter().apply {
+        write(command)
+        newLine()
+    }
+}
+
+/**
+ * Complete and close the [OutputStream] from [Process]
+ **/
+fun Process.commandEnd() = apply {
+    tryCatch { outputStream?.flush() }
+    tryCatch { outputStream?.close() }
+}
+
+/**
+ * Get the output of the [Process]
+ **/
+fun Process.getCommandOutputLines(): List<String>? {
+    var lines: List<String>? = null
+    try {
+        inputStream?.use { inputStream ->
+            try {
+                inputStream.bufferedReader().use { bufferedReader ->
+                    try {
+                        lines = bufferedReader.readLines()
+                    } catch (e: Exception) {  }
+                }
+            } catch (e: Exception) {  }
+        }
+    } catch (e: Exception) {  }
+    return lines
+}

c95661ad8b7bbcc065b7c5006858db5f7c4fca1a
1552980358 <1552980358@qq.com>
2021-07-13 21:13:07 +0800

    Update String.kt
    Signed-off-by: 1552980358 <1552980358@qq.com>

---
+++ src/main/kotlin/lib/github1552980358/ktExtension/jvm/kotlin/String.kt
@@ -0,0 +1,50 @@
+package lib.github1552980358.ktExtension.jvm.kotlin
+
+fun Int.pow(power: Int): Int {
+    var tmp = 1
+    repeat(power) {
+        tmp *= this
+    }
+    return tmp
+}
+
+class IllegalFormatException(message: String): IllegalArgumentException(message)
+
+/**
+ * Check whether the [String] is hex string
+ *
+ * @return [Boolean]
+ **/
+val String.isHexStr get(): Boolean {
+    repeat(length) { index ->
+        if (this[index] !in '0' .. '9' && this[index] !in 'A' .. 'F' && this[index] !in 'a' .. 'f') {
+            return false
+        }
+    }
+    return true
+}
+
+/**
+ * Convert hex contented [String] into [Int]
+ *
+ * @throws IllegalFormatException
+ * @return [Int]
+ **/
+val String.hexStrToInt: Int get() {
+    var tmp = 0
+    repeat(length) { index ->
+        tmp += when {
+            this[index] in '0' .. '9' -> {
+                (this[index].code - 48) * (16).pow(lastIndex - index)
+            }
+            this[index] in 'A' .. 'F' -> {
+                (this[index].code - 55) * (16).pow(lastIndex - index)
+            }
+            this[index] in 'a' .. 'f' -> {
+                (this[index].code - 87) * (16).pow(lastIndex - index)
+            }
+            else -> throw IllegalFormatException("Unknown character: ${this[index]}")
+        }
+    }
+    return tmp
+}

1d2f594a8dac983f6d89c42983cd7ef093998fb1
1552980358 <1552980358@qq.com>
2022-01-14 19:23:27 +0800

    ArrayList.kt: Add method add()
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- src/main/kotlin/lib/github1552980358/ktExtension/jvm/util/ArrayList.kt
+++ src/main/kotlin/lib/github1552980358/ktExtension/jvm/util/ArrayList.kt
@@ -156,6 +156,16 @@ fun ArrayList<Double>.binSearch(value: Double, sort: Boolean = false): Int {
         }
         mid = (first + last) / 2
     }
-    
+
     return if (this[mid] == value) mid else -1
 }
+
+/**
+ * Add an element with [parameters] and [apply] block of new instance of class [T]
+ **/
+inline fun <reified T> ArrayList<T>.add(vararg parameters: Any, apply: T.() -> Unit) = add(
+    when {
+        parameters.isEmpty() -> T::class.java.getDeclaredConstructor()
+        else -> T::class.java.getDeclaredConstructor(*parameters.map { it::class.java }.toTypedArray())
+    }.apply { isAccessible = true }.newInstance(parameters).apply(apply)
+)

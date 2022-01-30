b3537d9aafdc8bbe7a89e48b64c84747e3630e05
1552980358 <1552980358@qq.com>
2022-01-30 19:08:47 +0800

    List.kt: Rename package and merge
    Signed-off-by: 1552980358 <1552980358@qq.com>

--- src/main/kotlin/lib/github1552980358/ktExtension/jvm/kotlin/collections/List.kt
+++ src/main/kotlin/lib/github1552980358/ktExtension/jvm/kotlin/collections/List.kt
@@ -1,3 +1,14 @@
-package lib.github1552980358.ktExtension.jvm.util
+package lib.github1552980358.ktExtension.jvm.kotlin.collections
+
+/**
+ * Copy List
+ **/
+fun <T> List<T>.copy() = listOf(this).toMutableList()
+
+/**
+ * Copy a new [MutableList] and [shuffle] content elements
+ **/
+@Suppress("unused")
+fun <T> List<T>.copyAndShuffle() = copy().apply { shuffle() }
 
 fun <charSequence: CharSequence> List<charSequence>.joinTogether() = joinToString(separator = "")
